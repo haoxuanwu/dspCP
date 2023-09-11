5a972e91ae9191dfb54fb2462c01442ad0437b7f
Toryn Schafer <toryn.27@gmail.com>
2023-09-11 14:25:15 -0500

    exporting sample_mat

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -19,6 +19,21 @@ draw_indicators_generic <- function(res, r, n, w, m, s, J) {
     .Call('_dspCP_draw_indicators_generic', PACKAGE = 'dspCP', res, r, n, w, m, s, J)
 }
 
+#' Title
+#'
+#' @param row_ind
+#' @param col_ind
+#' @param val
+#' @param n
+#' @param num_entries
+#' @param linht
+#' @param rd
+#' @param D
+#'
+#' @return
+#' @export
+#'
+#' @examples
 sample_mat <- function(row_ind, col_ind, val, n, num_entries, linht, rd, D) {
     .Call('_dspCP_sample_mat', PACKAGE = 'dspCP', row_ind, col_ind, val, n, num_entries, linht, rd, D)
 }
